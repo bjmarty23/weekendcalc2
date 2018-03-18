a7893b553f3590715d7f447b2f689c1f6c883146
Britni Marty <bjmarty23@gmail.com>
2018-03-18 01:12:19 -0500

    post isnt working and now niether is get

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,18 +1,55 @@
 //Global Vars
-
 console.log('slickhead');
+
+
 
 $(document).ready(readyNow);
 function readyNow(){
+$('#add').on('click', );
     console.log('Js sourced');
 
-    $('#add').on('click', addition);
+}
+function numbersToPush() {
+    let inputx = $('#x').val();
+    $('#x').val('');
+    let inputy = $('#y').val();
+    $('#y').val('');
+    let numbersToSend = {inputOne: inputx,
+                         inputTwo: inputy};
+                         console.log(numbersToSend);
+
+$.ajax({
+    type: 'POST',
+    data: numbersToSend,
+    url: '/calc'
+
+}).done(function(response){
+    totalToDom();
+    console.log('success');
+}).fail(function(response){
+    alert('Somthing went WRONG');
+})
+}
+
+
+function totalToDom(){
+    $.ajax({
+        type: 'GET',
+        url: '/calc'
+
+    }).done(function(response){
+        //appendCalc(response);
+        //if another function need to be called put here
+    })
+}
+
+    //$('#add').on('click', addition);
     // $('#subtract').on('click', subtraction);
     // $('#multiply').on('click', mutiplication);
     // $('#divide').on('click', division);
-}
-function addition() {
-    addNum = parseInt($('#first').val()) + parseInt($('#second').val());
-    console.log(addNum);
-}
+
+
+
+
+
 console.log('bottom');
--- server/server.js
+++ server/server.js
@@ -1,27 +1,53 @@
 //Variables
-
+let num = [];
 let express = require('express');
 let app = express();
 const PORT = 5001;
 let bodyParser = require('body-parser');
 // this is need for jquery it check the code for the message 
+
 app.use(bodyParser.urlencoded({extended:true}));
+
 //use before the app.
 app.use(express.static('server/public'));
+
+function doMath (){
+    
+}
+
+
 //access the public folder in server folder.
 //get
-app.get('', (req,res) => {
-    res.send(object/array)
-});
 
 //post
-app.post('', (req,res) =>{
+
+
+app.get('/calc', (req,res) => {
+    // let NumebersToSend = req.body;
+    console.log(req.body, '31');
+    res.send(req.body);
+
+});
+
+app.post('/calc', (req,res) => {
+    console.log(req.body, '4');
+    let numbersToSend = req.body;
+    num.push(numbersToSend);
+    //console.log(num);
     res.sendStatus(200);
 });
 
+// console.log(req.body, '3');
+// let guessesToSend = req.body;//body.name & body.cost
+// allGuesses.push(guessesToSend);  
+// console.log(allGuesses, '2');
+// checkPlayers(guessesToSend);
+// res.sendStatus(200); 
+
 //listen 
 app.listen(PORT, () => {
-    console.log('server is running on port', + PORT);
+    console.log('server is running on port', PORT);
+    console.log(num);
 })
 
 
